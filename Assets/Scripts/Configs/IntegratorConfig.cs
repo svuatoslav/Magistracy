5fc04f3f435adfe0da60f7fafb2612d258feaefa
svuatoslav <moskalenko.sv.s@mail.ru>
2026-06-12 13:48:36 +0300

    Update Configs

--- Assets/Scripts/Configs/IntegratorConfig.cs
+++ Assets/Scripts/Configs/IntegratorConfig.cs
@@ -3,23 +3,23 @@
 [Serializable]
 public sealed class IntegratorConfig
 {
-    public double KeplerParam1 { get; }
-    public double KeplerParam2 { get; }
-    public double KeplerParam3 { get; }
+    public int ApproximationKeplerEquation { get; }
+    public int KeplerSolver { get; }
 
-    public double RKParam1 { get; }
-    public double RKParam2 { get; }
-    public double RKParam3 { get; }
+    public double RKIntegrationStep { get; }
+    public double RKEpsilon { get; }
+    public int RKSolver { get; }
+    public int RKError { get; }
 
     public IntegratorConfig(
-        double keplerParam1, double keplerParam2, double keplerParam3,
-        double rkParam1, double rkParam2, double rkParam3)
+        int approximationKeplerEquation, int keplerSolver,
+        double rkIntegrationStep, double rkEpsilon, int rkSolver, int rKError)
     {
-        KeplerParam1 = keplerParam1;
-        KeplerParam2 = keplerParam2;
-        KeplerParam3 = keplerParam3;
-        RKParam1 = rkParam1;
-        RKParam2 = rkParam2;
-        RKParam3 = rkParam3;
+        ApproximationKeplerEquation = approximationKeplerEquation;
+        KeplerSolver = keplerSolver;
+        RKIntegrationStep = rkIntegrationStep;
+        RKEpsilon = rkEpsilon;
+        RKSolver = rkSolver;
+        RKError = rKError;
     }
 }
--- Assets/Scripts/Configs/SimulationConfig.cs
+++ Assets/Scripts/Configs/SimulationConfig.cs
@@ -8,15 +8,22 @@ public sealed class SimulationConfig
     public OrbitConfig Orbit { get; }
     public IntegratorConfig Integrator { get; }
 
+    public int Time {  get; }
+    public int Scale { get; }
+
     public SimulationConfig(
         PlanetConfig planet,
         SatelliteConfig satellite,
         OrbitConfig orbit,
-        IntegratorConfig integrator)
+        IntegratorConfig integrator,
+        int time,
+        int scale)
     {
         Planet = planet;
         Satellite = satellite;
         Orbit = orbit;
         Integrator = integrator;
+        Time = time;
+        Scale = scale;
     }
 }
