056c5ee63f4020257c5889ed970c5a28fd9a8f3e
svuatoslav <moskalenko.sv.s@mail.ru>
2026-06-15 14:37:15 +0300

    Переписал методы решения Кеплера

--- Assets/Scripts/Kepler/DenbyMethod.cs
+++ Assets/Scripts/Kepler/DenbyMethod.cs
@@ -1,38 +1,27 @@
-﻿using static System.Math;
+﻿using System;
+using static System.Math;
 
 public sealed class DenbyMethod : KeplerEquationSolver
 {
     public DenbyMethod(double n, double e) : base(n, e) { }
 
-    public override double[] Calculate(int approximation, double[] time)
+    public override double E(double t, double En)
     {
-        double[] result = new double[time.Length];
-        NumberApproximation = approximation;
-        switch (NumberApproximation)
-        {
-            case 0:
-                for (int i = 0; i < result.Length; i++)
-                    result[i] = E0(time[i]);
-                return result;
-            case 1:
-                for (int i = 0; i < result.Length; i++)
-                    result[i] = E1(time[i]);
-                return result;
-            default:
-                for (int i = 0; i < result.Length; i++)
-                    result[i] = E1(time[i]);
-                for (approximation = 2; approximation <= NumberApproximation; approximation++)
-                    for (int i = 0; i < result.Length; i++)
-                    {
-                        if (result[i] - 2 * (M(time[i]) + e * Sin(result[i])) + M(time[i]) + e * Sin(M(time[i]) + e * Sin(result[i])) == 0)
-                            continue;
-                        result[i] = E(time[i], result[i]);
-                    }
-                return result;
-        }
+        if (CheckDivisor(t, En, out var divisor))
+            return En - Pow(M(t) + Eccentricity * Sin(En) - En, 2) / divisor;
+
+        throw new Exception("что-то пошло не так в методе Денди");
     }
 
-    public override double E(double t, double En) => En - Pow(M(t) + e * Sin(En) - En, 2) / (En - 2 * (M(t) + e * Sin(En)) + M(t) + e * Sin(M(t) + e * Sin(En)));
-    public override double E0(double t) => M(t) + 0.85 * e;
-    public override double E1(double t) => M(t) + e * (0.85 - Pow(Sin(M(t) + 0.85 * e) - 0.85, 2) / (0.85 - 2 * Sin(M(t) + 0.85 * e) + Sin(M(t) + e * Sin(M(t) + 0.85 * e))));
+    public override double E0(double t) => M(t) + 0.85 * Eccentricity;
+
+    private bool CheckDivisor(double t, double En, out double divisor)
+    {
+        divisor = En - 2 * (M(t) + Eccentricity * Sin(En)) + M(t) + Eccentricity * Sin(M(t) + Eccentricity * Sin(En));
+
+        if (divisor == 0)
+            throw new DivideByZeroException("Ошибка деления в методе Денди при вычислении эксцентрической аномалии");
+
+        return true;
+    }
 }
--- Assets/Scripts/Kepler/IterationMethod.cs
+++ Assets/Scripts/Kepler/IterationMethod.cs
@@ -4,41 +4,7 @@ public sealed class IterationMethod : KeplerEquationSolver
 {
     public IterationMethod(double n, double e) : base(n, e) { }
 
-    public override double[] Calculate(int approximation, double[] time)
-    {
-        double[] result = new double[time.Length];
-        NumberApproximation = approximation;
-        switch (NumberApproximation)
-        {
-            case 0:
-                for (int i = 0; i < result.Length; i++)
-                    result[i] = E0(time[i]);
-                return result;
-            case 1:
-                for (int i = 0; i < result.Length; i++)
-                    result[i] = E1(time[i]);
-                return result;
-            case 2:
-                for (int i = 0; i < result.Length; i++)
-                    result[i] = E2(time[i]);
-                return result;
-            case 3:
-                for (int i = 0; i < result.Length; i++)
-                    result[i] = E3(time[i]);
-                return result;
-            default:
-                for (int i = 0; i < result.Length; i++)
-                    result[i] = E3(time[i]);
-                for (approximation = 4; approximation <= NumberApproximation; approximation++)
-                    for (int i = 0; i < result.Length; i++)
-                        result[i] = E(time[i], result[i]);
-                return result;
-        }
-    }
+    public override double E(double t, double En) => M(t) + Eccentricity * Sin(En);
 
-    public override double E(double t, double En) => M(t) + e * Sin(En);
     public override double E0(double t) => M(t);
-    public override double E1(double t) => M(t) + e * Sin(M(t));
-    public double E2(double t) => M(t) + e * Sin(M(t) + e * Sin(M(t)));
-    public double E3(double t) => M(t) + e * Sin(M(t) + e * Sin(M(t) + e * Sin(M(t))));
 }
