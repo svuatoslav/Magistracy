11b7afd02ae78ab28c17e17c47edfe61de33209c
svuatoslav <moskalenko.sv.s@mail.ru>
2026-06-15 14:41:56 +0300

    Косметическое изменение

--- Assets/Scripts/Kepler/IterationMethod.cs
+++ Assets/Scripts/Kepler/IterationMethod.cs
@@ -1,10 +1,10 @@
-﻿using static System.Math;
+﻿using System;
 
 public sealed class IterationMethod : KeplerEquationSolver
 {
     public IterationMethod(double n, double e) : base(n, e) { }
 
-    public override double E(double t, double En) => M(t) + Eccentricity * Sin(En);
+    public override double E(double t, double En) => M(t) + Eccentricity * Math.Sin(En);
 
     public override double E0(double t) => M(t);
 }
